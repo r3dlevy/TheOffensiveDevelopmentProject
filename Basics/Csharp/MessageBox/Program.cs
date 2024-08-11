631731a2a5f1318e09b3561a8edbc1a1b4aab03d
R3dLevy <juvy@live.fr>
2024-08-11 20:44:43 +0200

    adding csharp/MessageBox csharp/ShellcodeRunner

---
+++ Basics/Csharp/MessageBox/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program
+{
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+
+    static void Main()
+    {
+        MessageBox(IntPtr.Zero, "Hello, world!", "My Message Box", 0);
+    }
+}
